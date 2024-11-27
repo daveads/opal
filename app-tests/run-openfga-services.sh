f89c36972fa6f0c4d19d21b28c6aeca7940f835b
daveads <the.daveads@gmail.com>
2024-11-27 14:21:27 +0100

    ..

--- app-tests/run-openfga-services.sh
+++ app-tests/run-openfga-services.sh
@@ -2,7 +2,7 @@
 
 # Start the services in detached mode
 echo "Starting OpenFGA and OPAL services..."
-docker compose -f docker-compose-app-tests-openfga.yml up -d
+docker compose -f app-tests/docker-compose-app-tests-openfga.yml up -d
 
 # Wait for services to initialize (adjust time if needed)
 echo "Waiting for services to initialize..."
